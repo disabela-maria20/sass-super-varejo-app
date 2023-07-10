4cfbda27cf770d1083b763452ae85be3d1e49fe7
Isabela <isabela10014@hotmail.com>
2023-07-10 17:53:28 -0300

    feat: :sparkles: add new page

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -14,7 +14,6 @@ Menu();
 
 document.addEventListener("DOMContentLoaded", () => {
   const imagem = document.querySelector(".logo");
-  console.log(window.location.pathname);
   if (
     window.location.pathname === "/" ||
     window.location.pathname === "/index.html"
@@ -48,3 +47,33 @@ document.addEventListener("DOMContentLoaded", () => {
     `categoria-${categoriaAtual}`
   );
 });
+
+const interestsArray = [
+  "APAS SHOW",
+  "Artigo",
+  "Atacarejo",
+  "Campanha",
+  "Consumo",
+  "E-Commerce",
+];
+const arr_select = [];
+
+for (const btn in interestsArray) {
+  const area_interests = document.querySelector(".area_interests");
+  const btn_interests = document.createElement("button");
+  area_interests.append(btn_interests);
+  btn_interests.innerText += interestsArray[btn];
+  btn_interests.classList.add("btn_interests");
+  btn_interests.addEventListener("click", (e) => {
+    e.preventDefault();
+    e.target.classList.toggle("active");
+    if (arr_select.includes(e.target.innerText)) {
+      const index = arr_select.indexOf(e.target.innerText);
+      arr_select.splice(index, 1);
+    } else {
+      arr_select.push(e.target.innerText);
+    }
+
+    console.log(arr_select);
+  });
+}
